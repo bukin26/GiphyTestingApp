d690472541e69c83a85b068d7d2a348ae2b83d0b
bukin26 <mg.bukin26@gmail.com>
2022-03-16 21:03:05 +0200

    added List Repository

---
+++ app/src/main/java/com/gmail/giphytestingapp/model/ListRepository.kt
@@ -0,0 +1,23 @@
+package com.gmail.giphytestingapp.model
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.gmail.giphytestingapp.model.entity.DataItem
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ListRepository @Inject constructor(
+    private val gifsService: GifsService
+) {
+    fun getPagedGifs(query: String): Flow<PagingData<DataItem>> {
+        return Pager(
+            config = PagingConfig(enablePlaceholders = false, pageSize = NETWORK_PAGE_SIZE),
+            pagingSourceFactory = { GifPagingSource(gifsService, query) }
+        ).flow
+    }
+
+    companion object {
+        private const val NETWORK_PAGE_SIZE = 20
+    }
+}
